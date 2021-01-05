a71705f58546e699244e408883e5b71c8f006ef5
Christopher Teddy <vitedocorp@yahoo.com>
2021-01-05 16:13:18 +0700

    Push

---
+++ app/src/main/java/com/example/liveattendances/views/ForgotPasswordActivity.kt
@@ -0,0 +1,25 @@
+package com.example.liveattendances.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.liveattendances.databinding.ActivityForgotPasswordBinding
+
+class ForgotPasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityForgotPasswordBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        init()
+    }
+
+    private fun init() {
+        setSupportActionBar(binding.tbForgotPassword)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
