51d48a91f289cb25a73351726e7d177ec3a5eb9f
Christopher Teddy <vitedocorp@yahoo.com>
2021-01-05 17:50:49 +0700

    History Fragment

--- app/src/main/java/com/example/liveattendances/views/fragment/AttendanceFragment.kt
+++ app/src/main/java/com/example/liveattendances/views/fragment/AttendanceFragment.kt
@@ -6,24 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.liveattendances.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class AttendanceFragment : Fragment(), OnMapReadyCallback  {
 
+    private var mapAttedance : SupportMapFragment ?= null
+    private var map: GoogleMap ?= null
 
-class AttendanceFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,20 +30,24 @@ class AttendanceFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
+        setupMaps()
     }
 
-    companion object {
+    private fun setupMaps() {
+        mapAttedance = childFragmentManager.findFragmentById(R.id.map_attendance) as SupportMapFragment
+        mapAttedance?.getMapAsync(this)
+    }
 
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AttendanceFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        val sydney = LatLng(-7.789302889219771, 110.36463041428438)
+        map!!.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+        map!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map!!.animateCamera(CameraUpdateFactory.zoomTo(20f))
     }
 }
