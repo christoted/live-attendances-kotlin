f058d8cd7966b55b15ba5bf87f6f5a25bc59050e
Christopher Teddy <vitedocorp@yahoo.com>
2021-01-09 17:31:13 +0700

    ProfielFragment

---
+++ app/src/main/java/com/example/liveattendances/model/LogoutResponse.kt
@@ -0,0 +1,8 @@
+package com.example.liveattendances.model
+
+import com.google.gson.annotations.SerializedName
+
+data class LogoutResponse(
+    @field:SerializedName("message")
+    val message: String? = null
+)
--- app/src/main/java/com/example/liveattendances/views/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/liveattendances/views/fragment/ProfileFragment.kt
@@ -1,22 +1,32 @@
 package com.example.liveattendances.views.fragment
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings.ACTION_LOCALE_SETTINGS
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.liveattendances.BuildConfig
 import com.example.liveattendances.MainActivity
 import com.example.liveattendances.databinding.FragmentAttendanceBinding
 import com.example.liveattendances.databinding.FragmentProfileBinding
+import com.example.liveattendances.dialog.MyDialog
+import com.example.liveattendances.hawkstorage.HawkStorage
+import com.example.liveattendances.model.LogoutResponse
+import com.example.liveattendances.networking.ApiServices
 import com.example.liveattendances.views.ChangePasswordActivity
 import com.example.liveattendances.views.login.LoginActivity
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private var binding: FragmentProfileBinding? = null
 
@@ -32,7 +42,17 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         onClick()
+        updateView()
     }
+
+    private fun updateView() {
+        val user = HawkStorage.instance(context).getUser()
+        val imageUrl = BuildConfig.BASE_IMAGE_URL + user.photo
+        Glide.with(context!!).load(imageUrl).into(binding!!.ivProfile)
+        binding?.tvNameProfile?.text = user.name
+        binding?.tvEmailProfile?.text = user.email
+    }
+
 
     private fun onClick() {
         binding?.btnChangePassword?.setOnClickListener {
@@ -40,13 +60,50 @@ class ProfileFragment : Fragment() {
         }
 
         binding?.btnChangeLanguage?.setOnClickListener {
-            context?.toast("Choose Language")
+            startActivity(Intent(ACTION_LOCALE_SETTINGS))
         }
 
         binding?.btnLogout?.setOnClickListener {
-            context?.startActivity<LoginActivity>()
-            (activity as MainActivity).finishAffinity()
+            AlertDialog.Builder(context)
+                .setTitle("Logout")
+                .setMessage("Are Your sure?")
+                .setPositiveButton("Yes") { dialog, _ ->
+                    logoutRequest(dialog)
+                }
+                .setNegativeButton("No") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .show()
         }
+    }
+
+    private fun logoutRequest(dialog: DialogInterface?) {
+        val token = HawkStorage.instance(context).getToken()
+        MyDialog.showProgressBar(context)
+        ApiServices.getLiveAttendanceServices().logoutRequest(token)
+            .enqueue(object : Callback<LogoutResponse> {
+                override fun onFailure(call: Call<LogoutResponse>, t: Throwable) {
+                    dialog?.dismiss()
+                    MyDialog.hideDialog()
+                    MyDialog.dynamicDialog(context, "Alert", "GAGAL ON Failure")
+                }
+
+                override fun onResponse(
+                    call: Call<LogoutResponse>,
+                    response: Response<LogoutResponse>
+                ) {
+                    dialog?.dismiss()
+                    MyDialog.hideDialog()
+                    if (response.isSuccessful) {
+                        HawkStorage.instance(context).deleteAll()
+                        (activity as MainActivity).finishAffinity()
+                        context?.startActivity<LoginActivity>()
+                    } else {
+                        MyDialog.dynamicDialog(context, "Alert", "Reponse tapi gagal")
+                    }
+                }
+
+            })
     }
 
     override fun onDestroyView() {
